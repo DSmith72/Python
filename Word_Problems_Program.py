49406cd184c5ac29d2ff7c83b0125fe5ad5fcd13
DSmith72 <40127193+DSmith72@users.noreply.github.com>
2020-12-27 21:28:27 -0500

    Add files via upload

---
+++ Word_Problems_Program.py
@@ -0,0 +1,38 @@
+# Word Problems Program 3/31/2020
+# David Smith
+# Demmonstrates Numbers and Math
+
+import math
+print ("""
+If a pregnant hippo, weighing 2,000 pounds, gives birth to a 100 pound calf, 
+but then eats 50 pounds of food, how much does she weigh? """)
+input("Press the enter key to find out.")
+print ("2000 - 100 + 50 = ",2000 - 100 + 50)
+
+print ("""
+If an adventurer returns from a successful quest and buys each of
+6 conpanions 3 bottles of ale, how many bottles does the adventurere buy? """)
+input("Press the enter key to find out.")
+print ("6 * 3 = ", 6 * 3)
+
+print ("""\nIf a kid has 24 pieces of Halloween candy and eats 6 pieces a day, 
+how many days will his stash last? """)
+
+input("Press the enter key to find out.")
+print ("24 / 6 = " ,math.floor(24 / 6), "days")
+
+print ("""
+If a group of 4 pirates finds a chest full of 107 coins, and
+they divide the booty evenly, how many coins will be left over?""")
+input("Press the enter key to find out.")
+print ("107 % 4 = ", 107 % 4, "coins left over")
+
+print ("""
+If a restaurant check comes to 19 dollars with tip, and you and
+your friends split it evenly 4 ways, how much do you each throw in? """)
+input("Press the enter key to find out.")
+print ("19 / 4 = ", 19 / 4)
+
+
+input("\n\nPress the enter key to exit.")
+
